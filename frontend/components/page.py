06d8cb280d4edd495c9b7dcabacf4bddc2aa0111
sr2mg <55787575+sr2mg@users.noreply.github.com>
2022-09-07 22:04:10 +0900

    comboに変更し、生成中は何回も押せなくした

--- frontend/components/page.py
+++ frontend/components/page.py
@@ -3,6 +3,7 @@ import PySimpleGUI as sg
 
 class Page:
     WIN_CLOSED = sg.WIN_CLOSED
+    STYLES = ['油画', '水彩', '粉笔画', '卡通', '儿童画', '蜡笔画', '探索无限']
 
     def __init__(self) -> None:
         sg.theme('DarkAmber')   # Add a touch of color
@@ -12,7 +13,10 @@ class Page:
 
     def getLayout(self):
         return [[sg.Text('prompt'), sg.InputText(key='prompt')],
-                [sg.Text('style'), sg.InputText(key='style')],
+                [sg.Text('style'), sg.Combo(
+                    self.STYLES, key='style', default_value='卡通', expand_x=True, readonly=True)],
+                [sg.Text('number of images'), sg.Slider(
+                    key='number_img', range=(1, 20), orientation='h')],
                 [sg.Button('Generate!', key="generate")],
                 [sg.Image(key='image0'), sg.Image(key='image1')],
                 [sg.Image(key='image2'), sg.Image(key='image3')],
@@ -24,3 +28,6 @@ class Page:
 
     def update_imageLayout(self, idx, image_data):
         self.window['image'+str(idx)].update(data=image_data)
+
+    def disabled_generate_button(self):
+        self.window['generate']

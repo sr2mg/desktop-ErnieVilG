d14406979f14979057c986242c58adbf646f6ec8
sr2mg <55787575+sr2mg@users.noreply.github.com>
2022-09-07 22:04:56 +0900

    複数枚生成できるように変更

--- start.py
+++ start.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+from faulthandler import disable
 from PIL import Image
 import io
 
@@ -5,21 +7,37 @@ from domain import hub_adaptor
 from frontend.components import page
 
 
-ha = hub_adaptor.hub_adaptor()
-# Create the Window
-frontend = page.Page()
-window = frontend.getWindow()
-#TODO: 非同期処理
-while True:
-    event, values = window.read()
-    if event == frontend.WIN_CLOSED:  # if user closes window or clicks cancel
-        break
-    if event == 'generate':
-        prompt = values['prompt']  # 初音未来,pixiv
-        style = values['style']  # 卡通
-        images = ha.get_image(prompt=prompt, style=style)
+class webappErnie:
+    def __init__(self) -> None:
+        self.ha = hub_adaptor.hub_adaptor()
+        self.frontend = page.Page()
+        self.window = self.frontend.getWindow()
+        pass
 
-        print(images)
+    def main(self):
+        # Create the Window
+
+        #TODO: 非同期処理
+        while True:
+            event, values = self.window.read()
+            if event == self.frontend.WIN_CLOSED:  # if user closes window or clicks cancel
+                break
+            if event == 'generate':
+                self.create_image(values['prompt'],
+                                  values['style'], values['number_img'])
+        self.window.close()
+
+    def create_image(self, prompt, style, number_img):
+        if(prompt == '' or style == ''):
+            return
+        # 大受欢迎的二维动画角色，可爱女孩子，原神的新角色，金色头发少女，pixiv排名第一,纤细的双手
+        self.window['generate'].update(disabled=True)
+        images = self.generate_images(number_img, prompt=prompt, style=style)
+        self.save_image(images)
+        print('You generated!')
+        self.window['generate'].update(disabled=False)
+
+    def save_image(self, images):
         for idx, image in enumerate(images):
             sg_image = image
             sg_image.thumbnail([512, 512])
@@ -28,6 +46,17 @@ while True:
             bio = io.BytesIO()
             sg_image.thumbnail([256, 256])
             sg_image.save(bio, format="PNG")
-            frontend.update_imageLayout(idx, bio.getvalue())
-        print('You generated!')
-window.close()
+            #self.frontend.update_imageLayout(idx, bio.getvalue())
+
+    def generate_images(self, number, prompt, style):
+        set = int(-(-number//6))  # 切り上げで何回回せばいいかを決める
+        ret_images = []
+        for idx in range(set):
+            images = self.ha.get_image(
+                prompt=prompt, style=style)
+            ret_images.extend(images)
+        return ret_images
+
+
+main = webappErnie()
+main.main()
